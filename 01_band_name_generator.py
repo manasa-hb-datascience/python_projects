1dcc089fa80488084dc307f3707210a0d2a2c3f9
Manasa HB <63460906+manasa-hb-datascience@users.noreply.github.com>
2026-05-19 08:14:23 +0530

    Delete 01_band_name_generator.py

--- 01_band_name_generator.py
+++
@@ -1,6 +0,0 @@
-## Project 1 - Band Name Generator
-
-print("Welcome to the Band Name Generator.")
-city = input("Whats the name of the city you grew up in?\n")
-pet = input("Whats your pet's name?\n")
-print(f"Your band name could be: {city} {pet}")
