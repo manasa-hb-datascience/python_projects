f71ffc1bec692736d732258bdbf606601aa66e87
Manasa H B <manasa.hb509@gmail.com>
2026-05-19 06:49:10 +0530

    added first python project

--- 1stproject.py
+++ 1stproject.py
@@ -1,1 +1,4 @@
-print("Hello Manasa, welcome to coding!")
+print("Welcome to the Band Name Generator.")
+city = input("Whats the name of the city you grew up in?\n")
+pet = input("Whats your pet's name?\n")
+print(f"Your band name could be: {city} {pet}")
